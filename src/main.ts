7658533f54ccf80aeae4cb5f9f6a04522c2c0467
Nguyen Duy Bach <nguyenduybach12a11@gmail.com>
2026-03-24 17:43:58 +0700

    config: template email authentication

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
